fe901b2b91d591f961e79d99d147b25c5e84ed83
denisagapov <denisagapov6666@gmail.com>
2024-02-25 15:14:13 +0100

    simple change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 5000
+const nodemailer = require("nodemailer");
 
 // create application/json parser
 app.use(bodyParser.json())
@@ -11,6 +12,31 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+app.post('/text-mail',(req,res)=>{
+  res.send('Hello World')
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: "densisagapov6666@gmail.com",
+      pass: "Climax123!2",
+      clientId: "804730404802-3ssulrqadtorcoitab8qr3ch4rb6n8g1.apps.googleusercontent.com",
+      clientSecret: "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",
+      refreshToken: "https://developers.google.com/oauthplayground"
+    }
+  });
+  const mailData = {
+    to:"denisagapov6666@gmail.com",
+    subject:"Hello",
+    text:"I'm denisagapov. Nice to meet you",
+  }
+  transporter.sendMail(mailData,(error,info)=>{
+    if(error){
+      return console.log(error)
+    }
+    res.status(200).send({message:"Mail send", message_id:info.message});
+  })
 })
 
 app.listen(port,()=>console.log(`server is running ${port}`));
