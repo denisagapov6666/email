778cea24e6b60866c582b52ba634c370944aa6e2
denisagapov <denisagapov6666@gmail.com>
2024-02-27 21:17:08 +0100

    complete email delivery

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 5000
-const nodemailer = require("nodemailer");
+const {CourierClient} = require('@trycourier/courier');
+
+const courier = new CourierClient({ authorizationToken: "dk_prod_VVQ43JZJQA45V8H2Y2QX3VN2V2MT" });
 
 // create application/json parser
 app.use(bodyParser.json())
@@ -13,30 +15,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.post('/text-mail',(req,res)=>{
-  res.send('Hello World')
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: "densisagapov6666@gmail.com",
-      pass: "Climax123!2",
-      clientId: "804730404802-3ssulrqadtorcoitab8qr3ch4rb6n8g1.apps.googleusercontent.com",
-      clientSecret: "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",
-      refreshToken: "https://developers.google.com/oauthplayground"
-    }
+app.post('/text-mail',async (req,res)=>{
+  const { requestId } = await courier.send({
+    message: {
+      to: {
+        email: "nicolas.edwards0822@gmail.com",
+      },
+      content: {
+        title: "Welcome!",
+        body: "Thanks for signing up, {{name}}",
+      },
+      data: {
+        name: "Peter Parker",
+      },
+      routing: {
+        method: "single",
+        channels: ["email"],
+      },
+    },
   });
-  const mailData = {
-    to:"denisagapov6666@gmail.com",
-    subject:"Hello",
-    text:"I'm denisagapov. Nice to meet you",
-  }
-  transporter.sendMail(mailData,(error,info)=>{
-    if(error){
-      return console.log(error)
-    }
-    res.status(200).send({message:"Mail send", message_id:info.message});
-  })
+  res.send(requestId);
+
 })
 
 app.listen(port,()=>console.log(`server is running ${port}`));
